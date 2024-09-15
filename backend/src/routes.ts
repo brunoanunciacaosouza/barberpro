42c5eb338f30332624907165b16165009dac5392
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-15 14:40:26 -0300

    criando modelos de cortes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,8 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { UserDetailController } from "./controllers/user/UserDetailController";
 import { UpdateUserController } from "./controllers/user/UpdateUserController";
 
+import { CreateHaircutController } from "./controllers/haircut/CreateHaircutController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -14,5 +16,8 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new UserDetailController().handle);
 router.put("/users", isAuthenticated, new UpdateUserController().handle);
+
+// routes haircut
+router.post("/haircut", isAuthenticated, new CreateHaircutController().handle);
 
 export default router;
