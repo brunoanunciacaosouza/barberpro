bf8d54175708f9faf03762c331a6dacb4c274ea8
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-19 15:18:28 -0300

    começando projeto

---
+++ barber-web/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BarberPro - Seu sistema completo</title>
+      </Head>
+      <div>
+        <h1>Barber Web</h1>
+      </div>
+    </>
+  );
+}
